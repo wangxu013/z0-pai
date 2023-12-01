e9db022dd18c72d62f235ec9b54ea96c51aaf103
wangxu013 <93688119+wangxu013@users.noreply.github.com>
2023-12-01 10:07:46 -1000

    #

--- zexp/public/javascripts/assets/serverNetAddress.js
+++ zexp/public/javascripts/assets/serverNetAddress.js
@@ -1,4 +1,4 @@
-var serverNetAddress ='http://66.91.14.118:5025';
-var serverIp = '66.91.14.118';
-var serverPort ='5025';
+var serverNetAddress ='http://http://34.125.102.25:3000';
+var serverIp = '34.125.102.25';
+var serverPort ='3000';
 var serverProtocol ='http';
